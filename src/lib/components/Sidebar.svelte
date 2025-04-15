7e4f2b61496832379c7be87d8097c385f9ab2496
Francisco Rivas <fran.workbench@gmail.com>
2025-04-15 18:00:39 -0400

    add: sidebar

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="w-12 h-screen sticky top-0 border-r border-gray-200">
+    <div>Hola</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
-
+	import Sidebar from '$lib/components/Sidebar.svelte';
+  
 	let { children } = $props();
-</script>
-
-<Navbar />
-{@render children()}
+  </script>
+  
+  <div class="flex min-h-screen">
+	<!-- Sidebar -->
+	<Sidebar />
+  
+	<!-- Contenido principal -->
+	<div class="flex-1 flex flex-col">
+	  <!-- Navbar fijo -->
+		<Navbar />
+  
+	  <!-- Área de contenido desplazable -->
+	  <main class="flex-1 overflow-y-auto p-6">
+		{@render children()}
+	  </main>
+	</div>
+  </div>
